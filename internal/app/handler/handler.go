e8a9f1e5e2d07fd5b2049cd40cf068c41cf097fb
andrey57x <57andreys57@gmail.com>
2025-09-07 09:33:02 +0300

    add application

---
+++ internal/app/handler/handler.go
@@ -0,0 +1,104 @@
+package handler
+
+import (
+	"Backend/internal/app/repository"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+)
+
+type Handler struct {
+	Repository *repository.Repository
+}
+
+func NewHandler(r *repository.Repository) *Handler {
+	return &Handler{
+		Repository: r,
+	}
+}
+
+func (h *Handler) GetReactions(ctx *gin.Context) {
+	var reactions []repository.Reaction
+	var err error
+
+	searchQuery := ctx.Query("query")
+	if searchQuery == "" {
+		reactions, err = h.Repository.GetReactions()
+		if err != nil {
+			logrus.Error(err)
+		}
+	} else {
+		reactions, err = h.Repository.GetReactionsByTitle(searchQuery)
+		if err != nil {
+			logrus.Error(err)
+		}
+	}
+
+	var calculationsCount int
+	var currCalculationId int
+	calculationsCount, currCalculationId, err = h.Repository.CurrentCalculation()
+	if err != nil {
+		logrus.Error(err)
+	}
+
+	ctx.HTML(http.StatusOK, "reactions.html", gin.H{
+		"reactions":                reactions,
+		"query":                    searchQuery,
+		"reactions_in_calculation": calculationsCount,
+		"mass_calculation_id":      currCalculationId,
+	})
+}
+
+func (h *Handler) GetReaction(ctx *gin.Context) {
+	idStr := ctx.Param("id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		logrus.Error(err)
+	}
+
+	reaction, err := h.Repository.GetReaction(id)
+	if err != nil {
+		logrus.Error(err)
+	}
+
+	var calculationsCount int
+	var currCalculationId int
+	calculationsCount, currCalculationId, err = h.Repository.CurrentCalculation()
+	if err != nil {
+		logrus.Error(err)
+	}
+
+	ctx.HTML(http.StatusOK, "single_reaction.html", gin.H{
+		"reaction": reaction,
+		"reactions_in_calculation": calculationsCount,
+		"mass_calculation_id":      currCalculationId,
+	})
+}
+
+func (h *Handler) GetMassCalculation(ctx *gin.Context) {
+	idStr := ctx.Param("id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		logrus.Error(err)
+	}
+
+	reactions, err := h.Repository.GetCalculationReactions(id)
+	if err != nil {
+		logrus.Error(err)
+	}
+
+	var calculationsCount int
+	var currCalculationId int
+	calculationsCount, currCalculationId, err = h.Repository.CurrentCalculation()
+	if err != nil {
+		logrus.Error(err)
+	}
+
+	ctx.HTML(http.StatusOK, "mass_calculation.html", gin.H{
+		"reactions": reactions,
+		"reactions_in_calculation": calculationsCount,
+		"mass_calculation_id":      currCalculationId,
+	})
+}
